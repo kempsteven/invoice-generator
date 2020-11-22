81f2e0ec9ea3187b89d5d681456125d0047f64d9
Kemp <frostkazuma@gmail.com>
2020-11-22 19:33:37 +0800

    make it better

--- app/Middleware/PrivateAuth.js
+++ app/Middleware/PrivateAuth.js
@@ -11,16 +11,16 @@ class PrivateAuth {
    * @param {Function} next
    */
   async handle ({ request, response }, next) {
-    // const privateKey = Env.get('PRIVATE_KEY')
-    // const validIps = Env.get('VALID_IPS').split(',')
-    // const requestPrivateKey = request.get().privateKey
-    // const ip = request.ip()
+    const privateKey = Env.get('PRIVATE_KEY')
+    const validIps = Env.get('VALID_IPS').split(',')
+    const requestPrivateKey = request.get().privateKey
+    const ip = request.ip()
 
-    // if (!validIps.includes(ip) || privateKey !== requestPrivateKey) { 
-    //   return response.status(400).send({
-    //     message: 'Authentication Failed.'
-    //   }) 
-    // }
+    if (!validIps.includes(ip) || privateKey !== requestPrivateKey) { 
+      return response.status(400).send({
+        message: 'Authentication Failed.'
+      }) 
+    }
 
     await next()
   }
